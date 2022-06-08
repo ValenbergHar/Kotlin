d2c913f13735ed88bc1dd4323c30405c8326f7a4
ValenbergHaradzienski <maskevich.valeri@gmail.com>
2022-06-08 18:13:09 +0300

    08.06.22

--- Courses/kotlin1/Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
+++ Courses/kotlin1/Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.example.firebase
 
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
+
+import android.widget.EditText
+
+import android.widget.ProgressBar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var verificationId: String? = null
+    private var progressBar: ProgressBar? = null
+    private var mAuth: FirebaseAuth? = null
+    private var phoneNo: EditText? = null
+    private var otp: EditText? = null
+    private var login: Button? = null
+    private var getotp: TextView? = null
+    private var otpsent: TextView? = null
+    private var resendOTP: TextView? = null
+    private var getotpclicked = false
+    private var countdowntimer: TextView? = null
+    var dialog: Dialog? = null
+    var user: FirebaseUser? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.hide()
 
-        val imgBtn = findViewById<ImageButton>(R.id.imageBtn)
-        imgBtn.setOnClickListener {
+        mAuth = FirebaseAuth.getInstance()
+        phoneNo = findViewById(R.id.txtPhone)
+        otp = findViewById(R.id.txtOtp)
+        login = findViewById(R.id.btnLogin)
 
+        login?.setOnClickListener {
+            loginOnClick()
         }
+
+    }
+
+    private fun loginOnClick() {
+        val number = phoneNo!!.text.toString().trim()
+        val otp1 = otp!!.text.toString().trim()
+
+        verifyCode(otp1);
+
+
+    }
+
+    private fun verifyCode(code: String) {
+        var credential: PhoneAuthCredential =PhoneAuthProvider.getCredential()
+
     }
 }
+
+
