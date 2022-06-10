aead5c3f63fef15af6a4c8a15ac00bfdf46b6c4b
ValenbergHaradzienski <maskevich.valeri@gmail.com>
2022-06-10 15:25:51 +0300

    10.06.22

--- Courses/kotlin1/Firebase/app/src/main/java/com/example/firebase/PhoneAuthentication.kt
+++ Courses/kotlin1/Firebase/app/src/main/java/com/example/firebase/PhoneAuthentication.kt
@@ -1,5 +1,7 @@
 package com.example.firebase
+
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,22 +9,21 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.FirebaseException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.*
 import java.util.concurrent.TimeUnit
 
 class PhoneAuthentication : AppCompatActivity() {
 
     lateinit var mAuth: FirebaseAuth
+    lateinit var mCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     lateinit var veriBtn: Button
     lateinit var authBtn: Button
     lateinit var phnNoTxt: EditText
     lateinit var verifiTxt: EditText
-    lateinit var mCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     lateinit var progress: ProgressBar
+    var verificationId =""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +36,30 @@ class PhoneAuthentication : AppCompatActivity() {
         verifiTxt = findViewById(R.id.verifiTxt)
         progress = findViewById(R.id.progress)
 
-        veriBtn.setOnClickListener { view: View? -> progress.visibility = View.VISIBLE
+        veriBtn.setOnClickListener { view: View? ->
+            progress.visibility = View.VISIBLE
             verify()
+        }
+        authBtn.setOnClickListener { view: View? ->
+            progress.visibility = View.INVISIBLE
+            authenticate()
         }
 
     }
 
+    private fun authenticate() {
+        val verifiNo = verifiTxt.text.toString()
+        val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(verificationId, verifiNo)
+        signIn(credential)
+    }
+
     private fun verificationCallbacks() {
         mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
                 Log.d(TAG, "onVerificationCompleted")
                 progress.visibility = View.INVISIBLE
+                signIn(credential)
+
 
             }
 
@@ -55,7 +69,28 @@ class PhoneAuthentication : AppCompatActivity() {
 
             override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
                 super.onCodeSent(p0, p1)
+                verificationId = p0.toString()
+                progress.visibility = View.INVISIBLE
 
+            }
+
+        }
+    }
+
+    private fun signIn(credential: PhoneAuthCredential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                toast("Logged in Successfully :)")
+                startActivity(Intent(this, MainActivity::class.java))
+                Log.d(TAG, "signInWithCredential:success")
+
+            } else {
+                // Sign in failed, display a message and update the UI
+                Log.w(TAG, "signInWithCredential:failure", task.exception)
+                if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                    // The verification code entered was invalid
+                }
+                // Update UI
             }
 
         }
@@ -65,14 +100,13 @@ class PhoneAuthentication : AppCompatActivity() {
 
         verificationCallbacks()
         val phnNo = phnNoTxt.text.toString()
-        val options =PhoneAuthOptions.newBuilder(mAuth)
+        val options = PhoneAuthOptions.newBuilder(mAuth)
             .setPhoneNumber(phnNo)
             .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
             .setActivity(this)                 // Activity (for callback binding)
             .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
             .build()
         PhoneAuthProvider.verifyPhoneNumber(options)
-
 
 
 //        PhoneAuthProvider.getInstance().verifyPhoneNumber(
@@ -82,5 +116,9 @@ class PhoneAuthentication : AppCompatActivity() {
 //            this,
 //            mCallbacks
 //        )
+    }
+
+    private fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
